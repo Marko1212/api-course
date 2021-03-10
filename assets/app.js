18ef024fd6511db0ee5e014e4776adce29393879
Marko1212 <shihonage7@gmail.com>
2021-03-10 11:26:20 +0100

    react app connected

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,20 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// les imports importants
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+const App = () => {
+    return <h1>Bonjour à tous !</h1>
+}
+
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App />, rootElement);
+
