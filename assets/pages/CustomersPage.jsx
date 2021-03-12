f84ff38b0849c4d225453a6debeb3534c94ed98c
Marko1212 <shihonage7@gmail.com>
2021-03-12 16:43:08 +0100

    first ajax request react towards symfony

--- assets/pages/CustomersPage.jsx
+++ assets/pages/CustomersPage.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const CustomersPage = (props) => {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/customers")
+      .then((response) => response.data["hydra:member"])
+      .then((data) => setCustomers(data))
+      .catch(error => console.log(error.response));
+  }, []);
+
   return (
     <>
       <h1>Liste des clients</h1>
@@ -17,21 +27,23 @@ const CustomersPage = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>18</td>
-            <td>
-              <a href="#">Lior Chamla</a>
-            </td>
-            <td>lior@sym.com</td>
-            <td>Lior Inc</td>
-            <td className="text-center">
-              <span className="badge badge-primary">4</span>
-            </td>
-            <td className="text-center">2 400,00 €</td>
-            <td>
-              <button className="btn btn-sm btn-danger">Supprimer</button>
-            </td>
-          </tr>
+          {customers.map((customer) => (
+            <tr key = {customer.id}>
+              <td>{customer.id}</td>
+              <td>
+                <a href="#">{customer.firstName} {customer.lastName}</a>
+              </td>
+              <td>{customer.email}</td>
+              <td>{customer.company}</td>
+              <td className="text-center">
+                <span className="badge badge-primary">{customer.invoices.length}</span>
+              </td>
+              <td className="text-center">{customer.totalAmount.toLocaleString()} €</td>
+              <td>
+                <button className="btn btn-sm btn-danger">Supprimer</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
