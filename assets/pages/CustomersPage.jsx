20c25286b1fa66dc3b6412643f33c06aa72ce924
Marko1212 <shihonage7@gmail.com>
2021-03-12 08:42:39 +0100

    updates

--- assets/pages/CustomersPage.jsx
+++ assets/pages/CustomersPage.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const CustomersPage = (props) => {
-    return ( <h1>Liste des clients</h1> );
+  return (
+    <>
+      <h1>Liste des clients</h1>
+    </>
+  );
 };
- 
+
 export default CustomersPage;
