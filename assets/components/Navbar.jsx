5ecec6e5963717a4bf777760279c427c7f324a33
Marko1212 <shihonage7@gmail.com>
2021-04-01 16:32:51 +0200

    liens NavLink

--- assets/components/Navbar.jsx
+++ assets/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AuthAPI from "../services/authAPI";
+import {NavLink} from "react-router-dom";
 
 const Navbar = (props) => { 
 
@@ -9,9 +10,9 @@ const Navbar = (props) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <NavLink className="navbar-brand" to="/">
         SymReact !
-      </a>
+      </NavLink>
       <button
         className="navbar-toggler"
         type="button"
@@ -27,20 +28,20 @@ const Navbar = (props) => {
       <div className="collapse navbar-collapse" id="navbarColor03">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <NavLink className="nav-link" to="/customers">
               Clients
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <NavLink className="nav-link" to="/invoices">
               Factures
-            </a>
+            </NavLink>
           </li>
         </ul>
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item"><a href="#" className="nav-link">Inscription</a></li>
+          <li className="nav-item"><NavLink to="/register" className="nav-link">Inscription</NavLink></li>
           <li className="nav-item">
-            <a href="#" className="btn btn-success">Connexion !</a>
+            <NavLink to="/login" className="btn btn-success">Connexion !</NavLink>
           </li>
           <li className="nav-item"><button onClick={handleLogout} href="#" className="btn btn-danger">Déconnexion</button></li>
         </ul>
