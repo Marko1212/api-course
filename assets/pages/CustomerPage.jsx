e2aa11bbd9054ca4b204d23becf1c31a6d182031
Marko1212 <shihonage7@gmail.com>
2021-04-10 14:57:34 +0200

    update

--- assets/pages/CustomerPage.jsx
+++ assets/pages/CustomerPage.jsx
@@ -1,72 +1,122 @@
-import React, { useState } from 'react';
-import Field from '../components/forms/Field';
-import {Link} from "react-router-dom";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Field from "../components/forms/Field";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const CustomerPage = (props) => {
+  const { id = "new" } = props.match.params;
 
-    const [customer, setCustomer ] = useState({
-        lastName: "Toto",
-        firstName: "",
-        email: "",
-        company: ""
-    });
+  const [customer, setCustomer] = useState({
+    lastName: "Toto",
+    firstName: "",
+    email: "",
+    company: "",
+  });
 
-    const [errors, setErrors] = useState({
-        lastName: "",
-        firstName: "",
-        email: "",
-        company: ""
-    });
+  const [errors, setErrors] = useState({
+    lastName: "",
+    firstName: "",
+    email: "",
+    company: "",
+  });
 
+  const [editing, setEditing] = useState(false);
 
-    const handleChange = ({currentTarget}) => {
-        const {name, value} = currentTarget;
-
-        setCustomer({...customer, [name]: value});
+  const fetchCustomer = async (id) => {
+    try {
+      const data = await axios
+        .get("http://localhost:8000/api/customers/" + id)
+        .then((response) => response.data);
+        console.log(data);
+    } catch(error) {
+        console.log(error.response);
     }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-           const response = await axios.post("http://localhost:8000/api/customers", customer);
-           setErrors({})
-          
-        } catch(error) {
-            if(error.response.data.violations) {
-                const apiErrors = {};
-                error.response.data.violations.forEach(violation => {
-                    apiErrors[violation.propertyPath] = violation.message;
-                });
+  useEffect(() => {
+    if (id !== "new") {
+      setEditing(true);
+      fetchCustomer(id);
+    }
+  }, [id]);
 
-                setErrors(apiErrors);
-            }
-        }
+  const handleChange = ({ currentTarget }) => {
+    const { name, value } = currentTarget;
 
-    };
+    setCustomer({ ...customer, [name]: value });
+  };
 
-    return ( 
-    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/customers",
+        customer
+      );
+      setErrors({});
+    } catch (error) {
+      if (error.response.data.violations) {
+        const apiErrors = {};
+        error.response.data.violations.forEach((violation) => {
+          apiErrors[violation.propertyPath] = violation.message;
+        });
+
+        setErrors(apiErrors);
+      }
+    }
+  };
+
+  return (
     <>
-    
-    <h1>Création d'un client</h1>
+      {(!editing && <h1>Création d'un client</h1>) || (
+        <h1>Modification du client</h1>
+      )}
 
-    <form onSubmit={handleSubmit}>
-        <Field name="lastName" label="Nom de famille" placeholder="Nom de famille du client" value={customer.lastName} onChange = {handleChange} error={errors.lastName}/>
-        <Field name="firstName" label="Prénom" placeholder="Prénom du client" value={customer.firstName} onChange = {handleChange} error={errors.firstName}/>
-        <Field name="email" label="Email" placeholder="Adresse email du client" type="email" value={customer.email} onChange = {handleChange} error={errors.email}/>
-        <Field name="company" label="Entreprise" placeholder="Entreprise du client" value={customer.company} onChange = {handleChange} error={errors.company}/>
+      <form onSubmit={handleSubmit}>
+        <Field
+          name="lastName"
+          label="Nom de famille"
+          placeholder="Nom de famille du client"
+          value={customer.lastName}
+          onChange={handleChange}
+          error={errors.lastName}
+        />
+        <Field
+          name="firstName"
+          label="Prénom"
+          placeholder="Prénom du client"
+          value={customer.firstName}
+          onChange={handleChange}
+          error={errors.firstName}
+        />
+        <Field
+          name="email"
+          label="Email"
+          placeholder="Adresse email du client"
+          type="email"
+          value={customer.email}
+          onChange={handleChange}
+          error={errors.email}
+        />
+        <Field
+          name="company"
+          label="Entreprise"
+          placeholder="Entreprise du client"
+          value={customer.company}
+          onChange={handleChange}
+          error={errors.company}
+        />
 
         <div className="form-group">
-            <button className="submit btn btn-success">Enregistrer</button>
-            <Link to="/customers" className="btn btn-link">Retour à la liste</Link>
+          <button className="submit btn btn-success">Enregistrer</button>
+          <Link to="/customers" className="btn btn-link">
+            Retour à la liste
+          </Link>
         </div>
-    </form>
-    
-    </> 
-    
-    );
+      </form>
+    </>
+  );
 };
- 
+
 export default CustomerPage;
