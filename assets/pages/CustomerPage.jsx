300b231e73a0eef7ed132673bc4b3b69d7c7e838
Marko1212 <shihonage7@gmail.com>
2021-04-10 09:33:43 +0200

    formulaire

--- assets/pages/CustomerPage.jsx
+++ assets/pages/CustomerPage.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Field from '../components/forms/Field';
 import {Link} from "react-router-dom";
+import axios from 'axios';
 
 const CustomerPage = (props) => {
+
+    const [customer, setCustomer ] = useState({
+        lastName: "Toto",
+        firstName: "",
+        email: "",
+        company: ""
+    });
+
+    const [errors, setErrors] = useState({
+        lastName: "",
+        firstName: "",
+        email: "",
+        company: ""
+    });
+
+
+    const handleChange = ({currentTarget}) => {
+        const {name, value} = currentTarget;
+
+        setCustomer({...customer, [name]: value});
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+           const response = await axios.post("http://localhost:8000/api/customers", customer);
+           console.log(response.data);
+        } catch(error) {
+            console.log(error.response);
+        }
+
+    }
+
     return ( 
     
     <>
     
     <h1>Création d'un client</h1>
 
-    <form>
-        <Field name="lastName" label="Nom de famille" placeholder="Nom de famille du client" />
-        <Field name="firstName" label="Prénom" placeholder="Prénom du client" />
-        <Field name="email" label="Email" placeholder="Adresse email du client" type="email" />
-        <Field name="company" label="Entreprise" placeholder="Entreprise du client" />
+    <form onSubmit={handleSubmit}>
+        <Field name="lastName" label="Nom de famille" placeholder="Nom de famille du client" value={customer.lastName} onChange = {handleChange} error={errors.lastName}/>
+        <Field name="firstName" label="Prénom" placeholder="Prénom du client" value={customer.firstName} onChange = {handleChange} error={errors.firstName}/>
+        <Field name="email" label="Email" placeholder="Adresse email du client" type="email" value={customer.email} onChange = {handleChange} error={errors.email}/>
+        <Field name="company" label="Entreprise" placeholder="Entreprise du client" value={customer.company} onChange = {handleChange} error={errors.company}/>
 
         <div className="form-group">
             <button className="submit btn btn-success">Enregistrer</button>
