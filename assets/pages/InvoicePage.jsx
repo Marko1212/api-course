9af0985e6e9f76759435c2e2dd2813adead6266b
Marko1212 <shihonage7@gmail.com>
2021-04-10 21:11:19 +0200

    suite invoices

--- assets/pages/InvoicePage.jsx
+++ assets/pages/InvoicePage.jsx
@@ -1,19 +1,71 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Field from "../components/forms/Field";
+import Select from "../components/forms/Select";
 
 const InvoicePage = (props) => {
+  const [invoice, setInvoice] = useState({
+    amount: "",
+    customer: "",
+    status: "",
+  });
 
-    const [invoice, setInvoice] = useState({
-        amount: "",
-        customer: "",
-        status: ""
-    });
-    
+  const [errors, setErrors] = useState({
+    amount: "",
+    customer: "",
+    status: "",
+  });
+
+  // Gestion des changements des inputs dans le formulaire
+  const handleChange = ({ currentTarget }) => {
+    const { name, value } = currentTarget;
+
+    setInvoice({ ...invoice, [name]: value });
+  };
+
   return (
     <>
       <h1>Création d'une facture</h1>
       <form>
-          <Field name="amount" type="number" placeholder="Montant de la facture" label="Montant" onChange={handleChanges} value={} />
+        <Field
+          name="amount"
+          type="number"
+          placeholder="Montant de la facture"
+          label="Montant"
+          onChange={handleChange}
+          value={invoice.amount}
+          error={errors.amount}
+        />
+
+        <Select
+          name="customer"
+          label="Client"
+          value={invoice.customer}
+          error={errors.customer}
+          onChange={handleChange}
+        >
+          <option value="1">Marko Askovic</option>
+          <option value="2">Olga Askovic</option>
+        </Select>
+
+        <Select
+          name="status"
+          label="Statut"
+          value={invoice.status}
+          error={errors.status}
+          onChange={handleChange}
+        >
+          <option value="SENT">Envoyée</option>
+          <option value="PAID">Payée</option>
+          <option value="CANCELLED">Annulée</option>
+        </Select>
+
+        <div className="form-group">
+          <button type="submit" className="btn btn-success">
+            Enregistrer
+          </button>
+          <Link to="/invoices" className="btn btn-link">Retour aux factures</Link>
+        </div>
       </form>
     </>
   );
